6bf89a993f0559c2947b56faa8cbcea5bc0dad0f
SekuShijiree <83066241+SekuShijiree@users.noreply.github.com>
2021-05-13 13:31:25 +0800

    Add files via upload

---
+++ Less 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"hello Seku"<<endl;
+	cout<<"hello Seku"<<endl;
+	cout<<"hello Seku"<<endl;
+	cout<<"hello Seku";
+		
+	
+	return 0;	
+}
+
+
+
+
+	
+	
---
+++ Less 2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int x = 15, y = 123; //integer buhel too backslash //
+
+	
+	cout<<"x="<<x<<endl<<"y="<<y;
+
+	return 0;	
+}
+
+
+
+
+	
